33d4635823138f5cd71a32006297c04530520c11
SharProd <Sasha.sharai1998@mail.ru>
2022-05-04 19:48:45 +0300

    All home work

--- home_2_1.py
+++ home_2_1.py
@@ -2,15 +2,11 @@
 #Составьте список и кортеж с этими числами.
 # один из вариантов решения
 
-# пробовал метод split(), в данном случае он не коректно работает,
-# а именно ему нужен разделитель, если его не указать( split()) получается ["строка"],
-# а если указать ("") в разделителе , то выбивает ошибку. Метод хорош при разбиении по словам.
 
 input_string = input("write number: ")
-input_string_in_list = list()
+input_string_in_list = input_string.split(",")
 
-for item in input_string:
-    input_string_in_list.append(item)
+
 
 input_string_in_tuple = tuple(input_string_in_list)
 
