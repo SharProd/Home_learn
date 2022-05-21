1717872ec0225ab834f72e590999de7554cafca1
SharProd <Sasha.sharai1998@mail.ru>
2022-05-21 18:41:57 +0300

    домашние задания 6 урок

---
+++ home_6_4.py
@@ -0,0 +1,29 @@
+""" Написать программу, которая посчитает кол-во одинаковых элементов в списке.
+ Список будет заполняться рандомными целыми числами.
+ Рекомендую использовать несколько функций
+ (для заполнения списка целыми числами, для подсчета количества, для вывода)"""
+from random import random
+
+def edit_list(len_list):
+    x_list = []
+    for i in range(len_list):
+        x_list.append(int(random()*100))
+    return  x_list
+
+def count_list_number(lisT):
+    counter=dict()
+    for i in lisT:
+       if lisT.count(i)>1:
+           counter[i]=lisT.count(i)
+    return counter
+
+def show(dicT):
+    text = f'повторяющееся элементы:\n'
+    if len(dicT)>0:
+        for i in dicT.keys():
+            text+=f'{i} - количество {dicT[i]} \n'
+    else:
+        text = ' нет повторяющихся элементов'
+    return text
+
+print(show(count_list_number(edit_list(100))))
